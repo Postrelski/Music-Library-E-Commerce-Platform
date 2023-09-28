725340ba5441ecdde585f3b58d5d0f25d6e34d66
Matthew Postrel <matthewpostrel@gmail.com>
2023-09-28 12:13:30 -0400

    implemented elastic carousel

--- synthhacker_remake/src/components/ProductList.js
+++ synthhacker_remake/src/components/ProductList.js
@@ -2,86 +2,39 @@ import Product from "./Product.js";
 import "./ProductList.css";
 import pic from "../assets/synth.png";
 import { useState } from "react";
+import React, { Component } from "react";
+import Carousel from "react-elastic-carousel";
 
 function ProductList(props) {
   const slides = [
-    { title: "Product 1", price: 10, href: "/item", pic: { pic } },
-    { title: "Product 2", price: 15, href: "/item2", pic: { pic } },
-    { title: "Product 3", price: 5, href: "/item3", pic: { pic } },
+    { title: "Product 1", price: 10, href: "/item", pic: pic, key: 1 },
+    { title: "Product 2", price: 15, href: "/item2", pic: pic, key: 2 },
+    { title: "Product 3", price: 5, href: "/item3", pic: pic, key: 3 },
+    { title: "Product 4", price: 5, href: "/item3", pic: pic, key: 4 },
   ];
-  const [slide, setSlide] = useState(0);
 
-  function slideHanlderRight() {
-    if (slide + 1 == 2) {
-      setSlide(0);
-      dotHandlerLeft();
-    } else {
-      setSlide(slide + 1);
-      dotHandlerRight();
-    }
-  }
-
-  function slideHanlderLeft() {
-    if (slide == 0) {
-      setSlide(1);
-      dotHandlerRight();
-    } else {
-      setSlide(slide - 1);
-      dotHandlerLeft();
-    }
-  }
-
-  const [dots, setDots] = useState("fas");
-  const [dots2, setDots2] = useState("far");
-
-  function dotHandlerLeft() {
-    setSlide(0);
-    setDots("fas");
-    setDots2("far");
-  }
-
-  function dotHandlerRight() {
-    setSlide(1);
-    setDots("far");
-    setDots2("fas");
-  }
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 900, itemsToShow: 2 },
+    { width: 1300, itemsToShow: 3 },
+    { width: 1700, itemsToShow: 4 },
+  ];
 
   return (
-    <>
-      <div className="items">
-        <i
-          className="arrow fa-solid fa-arrow-left"
-          onClick={slideHanlderLeft}
-        />
-        <Product
-          title={slides[slide].title}
-          price={slides[slide].price}
-          pic={pic}
-          href={slides[slide].href}
-        />
-
-        <Product
-          title={slides[slide + 1].title}
-          price={slides[slide + 1].price}
-          pic={pic}
-          href={slides[slide + 1].href}
-        />
-
-        <i
-          className="arrow fa-solid fa-arrow-right"
-          onClick={slideHanlderRight}
-        />
-      </div>
-      <div className="dots">
-        <i className={`${dots} fa-dot-circle`} onClick={dotHandlerLeft} />
-        <i className={`${dots2} fa-dot-circle`} onClick={dotHandlerRight} />
-      </div>
-    </>
+    <div className="slider-container">
+      <Carousel breakPoints={breakPoints}>
+        {slides.map((item) => (
+          <Product
+            className="product"
+            title={item.title}
+            key={item.key}
+            price={item.price}
+            href={item.href}
+            pic={item.pic}
+          />
+        ))}
+      </Carousel>
+    </div>
   );
 }
 export default ProductList;
-
-{
-  /* <Product title="Product2" price="15" pic={pic} href="/item2" />
-<Product title="Product3" price="5" pic={pic} href="/item3" /> */
-}
