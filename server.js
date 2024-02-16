b302b29875e31c272f2543e9ff2ec686c2ec85a0
Matthew Postrel <matthewpostrel@gmail.com>
2024-02-16 13:43:20 -0500

    added some changes to the JSON files

--- server.js
+++ server.js
@@ -3,11 +3,31 @@ var cors = require("cors");
 const stripe = require("stripe")(
   "sk_test_51OjQ0pLgfwAAD6ezr9qDS1UumAoGSHVBEzmq4gWl0QpDqtnw7Ao7ZIkVgx2ARLlWQwzgg9J92wCUnIU6zFdXXANG00OHFfHB6H"
 );
-
 const app = express();
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
+
+// add this ->
+// MIDDLEWARE
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "https://shrouded-journey-38552.heroku...",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 
 app.post("/checkout", async (req, res) => {
   console.log(req.body);
@@ -28,15 +48,22 @@ app.post("/checkout", async (req, res) => {
     cancel_url: "http://localhost:3000/cancel",
   });
 
-  // window.localStorage.clear();
   res.send(
     JSON.stringify({
       url: session.url,
     })
   );
-
-  // clear the local storage after succesful payment?
-  // window.localStorage.clear();
 });
+
+// add this ->
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(4000, () => console.log("Listening on port 4000!"));
